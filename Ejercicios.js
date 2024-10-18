be48d926004a5ab6d446a197d1aaec21cbc71b17
Alexander <adragar997@g.educaand.es>
2024-10-18 10:05:05 +0200

    Subir nuevos ejercicios y modificar algunos

--- Ejercicios.js
+++ Ejercicios.js
@@ -16,6 +16,23 @@ let c1 = document.getElementById("c1")
 let c2 = document.getElementById("c2")
 let c3 = document.getElementById("c3")
 
+let bloque1 = document.getElementById("ficha1")
+let bloque2 = document.getElementById("ficha2")
+let bloque3 = document.getElementById("ficha3")
+
+let boton = document.getElementById("boton")
+
+let letras = document.getElementById("letras")
+
+window.addEventListener('scroll',function(event){
+    console.log(divHover.getBoundingClientRect().y)
+    if (divHover.getBoundingClientRect().y > window.innerHeight*25/100) {
+        boton.style.display = "none"
+    } else {
+        boton.style.display = "block"
+    }
+})
+
 window.onload = function (){
     cuadro.addEventListener('mouseover',function(){
         cuadro.style.backgroundColor = "red"
@@ -52,14 +69,36 @@ botonCalcular.addEventListener('click',function (){
 
 c1.addEventListener('mouseover',function(){
     colorAdquirido = c1.getAttribute('style')
-    c2.setAttribute('style',colorAdquirido)
 })
 
 c2.addEventListener('mouseout',function(){
     c2.setAttribute('style','background : white')
+    colorAdquirido = ""
+    c2.addEventListener('mouseover',function(){
+        c2.setAttribute('style',colorAdquirido)
+    })
 })
 
 c3.addEventListener('mouseover',function(){
     colorAdquirido = c3.getAttribute('style')
-    c2.setAttribute('style',colorAdquirido)
 })
+
+function mostrarBloques(tecla){
+    if (tecla.code == "Digit1"){
+        bloque1.style.display = "block"
+    }
+    if (tecla.code == "Digit2"){
+        bloque2.style.display = "block"
+    }
+    if (tecla.code == "Digit3"){
+        bloque3.style.display = "block"
+    }
+}
+
+function comprobacion(evento){
+    if (evento.code.startsWith("D")) {
+        return false
+    } else {
+        return true
+    }
+}
